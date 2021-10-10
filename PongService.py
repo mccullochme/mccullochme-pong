14fe566fe194c1a5c4c4e50ad8fb2725710da6c5
Max McCulloch <max.mcculloch.prof@gmail.com>
2021-10-10 19:44:52 -0400

    fixed stuuf

--- PongService.py
+++ PongService.py
@@ -3,12 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 import requests
 from requests.auth import HTTPDigestAuth
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,11 +16,9 @@ def index():
 
 @app.route('/pong', methods=['GET'])
 def pong():
-    #return rand int
-    return '<h1> hello </h1>'
+    num = random.randint(0, 9)
+    return '<h1>' + num + '</h1>'
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 
